5a26e1ca575a5c86ca98296d2049fc6e4803228c
Prakash Pandey <pkpndy@gmail.com>
2025-06-30 09:08:53 +0530

    25

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -11,7 +11,8 @@
 class Solution {
 private:
     ListNode* mergeList(ListNode* l1, ListNode* l2) {
-        ListNode dummy;
+        ListNode dummy; //taking dummy node to handle head==nullptr edge case
+        //so in the end if the head==nullptr then returning dummy.next will return nullptr
         ListNode* tail = &dummy;
 
         while(l1 && l2) {
@@ -19,35 +20,35 @@ private:
                 tail->next = l1;
                 l1=l1->next;
             } else {
-                tail->next=l2;
+                tail->next = l2;
                 l2=l2->next;
             }
-            tail = tail->next;
+            tail=tail->next;
         }
-
         if(l1) {
-            tail->next = l1;
+            tail->next=l1;
         }
         if(l2) {
-            tail->next = l2;
+            tail->next=l2;
         }
         return dummy.next;
     }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.empty()) {
-            return nullptr;
-        }
+        if(lists.empty())   return nullptr; //if there are no lists
 
-        while(lists.size() > 1) {
+        while(lists.size() > 1) { //if there are atleast two lists
             vector<ListNode*> mergedLists;
-            for(int i=0; i < lists.size(); i+=2) {
-                ListNode* l1 = lists[i];
-                ListNode* l2 = (i+1) < lists.size() ? lists[i+1] : nullptr;
-                mergedLists.push_back(mergeList(l1, l2));
+            for(int i=0; i<lists.size(); i+=2) { //taking two lists in each iteration
+                ListNode* l1 = lists[i]; //taking the first list in this iteration
+                
+                // checking if the next to current list there is list or not
+                //(odd number of lists case)
+                ListNode* l2 = i+1 < lists.size() ? lists[i+1] : nullptr;
+                mergedLists.push_back(mergeList(l1, l2)); //pushing the result of the merging
             }
-            lists = mergedLists;
+            lists = mergedLists; //putting the merged list into the lists
         }
-        return lists[0];
+        return lists[0]; //since there will be only one final merged list
     }
 };
---
+++ 25. Reverse Nodes in k-Group.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy = new ListNode(0, head); //creating the first dummy node with head as next
+        ListNode* groupPrev = dummy; //pointing the end of previous group at this dummy
+
+        while(true) {
+            ListNode* kth = getKth(groupPrev, k);
+            if(!kth) { //if the group end but we didn't find k in during any iteration
+                break; //break as we don't need to reverse as this group doesn't have k nodes
+            }
+            ListNode* groupNext = kth->next; //next group starts after kth node
+            //below nodes will be used for traversal
+            ListNode* curr = groupPrev->next; //current group will start after previous group
+            ListNode* prev = kth->next; //we point prev at the next group start
+            while(curr != groupNext) { //we loop till curr doesn't reach next group start
+                ListNode* tmp = curr->next;//we will change the next of the current,so we store it
+                curr->next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+            ListNode* tmp = groupPrev->next; //pointing the tmp at the old head
+            //kth node was the last node of the current group,
+            //so it came at the first place after reversal
+            groupPrev->next = kth; //we point kth node as the start of the current group
+            // since old head was the first node of the group and
+            //now it is the last node of the current group
+            groupPrev = tmp; //we make it as the last node of the current group
+            //which will become previous in the next iteration
+        }
+        return dummy->next;
+    }
+
+    ListNode* getKth(ListNode* curr, int k) {
+        while(curr && k > 0) { //run the loop only if curr != nullptr && k > 0
+            curr = curr->next; //move the pointer till kth node
+            k--;
+        }
+        return curr; //return the pointer at which the current group ends
+    }
+};
