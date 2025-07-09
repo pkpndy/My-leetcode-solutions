cda1affd01a2e849485d493f28cf62a4d18a1467
Prakash Pandey <pkpndy@gmail.com>
2025-07-09 19:36:10 +0530

    added space optimised solution

--- 746. Min Cost Climbing Stairs.cpp
+++ 746. Min Cost Climbing Stairs.cpp
@@ -24,18 +24,33 @@ public:
         int minCostClimbingStairs(vector<int>& cost) {
             int n = cost.size();
 
-            if(n==0)    return 0;
-            if(n==1)    return 0;
-
             vector<int> dp(n+1);
             dp[0] = 0;
             dp[1] = 0;
 
             for(int i=2; i<=n; i++) {
+                //current minimum cost would be minimum cost of previous or previous of previous
                 dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
             }
 
             return dp[n];
+        }
+
+        //This is space optimised solution
+        int minCostClimbingStairs(vector<int>& cost) {
+            int n = cost.size();
+            
+            int prev2 = 0;
+            int prev = 0;
+            int curr = 0;
+
+            for(int i=2; i<=n; i++) {
+                curr = min(prev + cost[i-1], prev2 + cost[i-2]);
+                prev2 = prev;
+                prev = curr;
+            }
+
+            return curr;
         }
         */
 };
