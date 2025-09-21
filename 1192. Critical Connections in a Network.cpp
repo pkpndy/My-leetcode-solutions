e68b496b26ee3a6af66bdc6b8922e82a6f3fb965
Prakash Pandey <pkpndy@gmail.com>
2025-09-21 14:22:50 +0530

    1192 comments

--- 1192. Critical Connections in a Network.cpp
+++ 1192. Critical Connections in a Network.cpp
@@ -2,25 +2,25 @@ class Solution {
 private:
     int timer=1;
 public:
-// The Memory Limit Exceeded (MLE) isn’t because Tarjan’s algorithm itself is heavy—it’s linear in V+E. 
-// The problem is in how you’re passing parameters.
-
-// Every recursive call copies the entire adjacency list. adjList, hence pass it by reference
+// Every recursive call copies the entire adjacency list, if you pass it by value.
 // That’s O(V+E) memory per recursion call → easily blows memory.
     void dfs(int node, int parent, vector<int>& vis, vector<vector<int>>& adjList, 
     int tin[], int low[], vector<vector<int>>& bridges) {
         vis[node]=1;
-        tin[node] = low[node] = timer;
-        timer++;
-        for(auto it: adjList[node]) {
-            if(it == parent)    continue;
-            if(vis[it] == 0) {
-                dfs(it, node, vis, adjList, tin, low, bridges);
-                low[node] = min(low[it], low[node]);
+        tin[node] = low[node] = timer; //on reaching a node we update the time taken and lowest time
+        timer++; //increase the timer
+        for(auto it: adjList[node]) { //explore all the adjacent nodes
+            if(it == parent)    continue; //if the adjacent node is parent then ignore
+            if(vis[it] == 0) { //if the adjacent node is not visited
+                dfs(it, node, vis, adjList, tin, low, bridges); //make the dfs call for this node
+                low[node] = min(low[it], low[node]); //we store the lowest time to reach this node
+                //if the lowest time taken to reach adjacent node is greater than 
+                //time taken to reach the current node
                 if(low[it] > tin[node]) {
-                    bridges.push_back({node, it});
+                    bridges.push_back({node, it}); //then its a bridge
                 }
             } else {
+                //even if the adjacent node is already visited we still store the lowest time to reach 
                 low[node] = min(low[it], low[node]);
             }
         }
@@ -28,18 +28,18 @@ public:
 
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         vector<vector<int>> adjList(n);
-        for(auto it: connections) {
+        for(auto it: connections) { //we first create a graph out of the edges
             int u = it[0];
             int v = it[1];
             adjList[u].push_back(v);
-            adjList[v].push_back(u);
+            adjList[v].push_back(u); //since it is an undirected graph
         }
 
-        vector<int> vis(n, 0);
-        int tin[n];
-        int low[n];
-        vector<vector<int>> bridges;
-        dfs(0, -1, vis, adjList, tin, low, bridges);
+        vector<int> vis(n, 0); //visited array, since we are going to do dfs traversal
+        int tin[n]; //time/steps taken to reach a node (time of insertion into the call stack)
+        int low[n]; //lowest time of all adjacent nodes except the parent
+        vector<vector<int>> bridges; //store the final bridges found
+        dfs(0, -1, vis, adjList, tin, low, bridges); //first node and its parent
         return bridges;
     }
 };
