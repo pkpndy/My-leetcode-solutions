acd8854d24b2a018786b7a02fa127035516a8a86
Prakash Pandey <pkpndy@gmail.com>
2026-01-05 23:53:59 +0530

    24

--- 24. Swap Nodes in Pairs.cpp
+++ 24. Swap Nodes in Pairs.cpp
@@ -21,17 +21,17 @@ public:
         // ListNode* dummy = new ListNode(0, head);
         // ListNode* prev = dummy;
 
-        while(prev->next && prev->next->next) {
-            ListNode* first = prev->next;
-            ListNode* second = first->next;
+        while(prev->next && prev->next->next) { //loop will run till pair of nodes is left
+            ListNode* first = prev->next; //first node
+            ListNode* second = first->next; //second node
 
             //swap
-            first->next=second->next;
-            second->next=first;
-            prev->next = second;
+            first->next=second->next; //connect first node to the next node of the second
+            second->next=first;  //connect second node to the first so that it goes back
+            prev->next = second; //connect the prev node to this newly moved second node
 
             //move prev to the previous of next pair to be reversed
-            prev=first;
+            prev=first; //since first node moved ahead (that was earlier the place of second)
         }
         return dummy.next;
     }
