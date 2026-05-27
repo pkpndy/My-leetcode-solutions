db915fbfcb7ff763734c64dbb14f151e31c5338c
Prakash Pandey <pkpndy@gmail.com>
2026-05-27 20:38:19 +0530

    Enhance reverseStack function with comments
    Added comments explaining two methods for inserting the top element at the bottom of the stack during reversal.

--- Reverse stack using recursion.cpp
+++ Reverse stack using recursion.cpp
@@ -1,7 +1,3 @@
-#include<iostream>
-#include<stack>
-
-using namespace std;
 
 void insertAtBottom(stack<int>& st, int element) {
     if(st.empty()) {
@@ -15,11 +11,28 @@ void insertAtBottom(stack<int>& st, int element) {
 }
 
 void reverseStack(stack<int> &st) {
-    if(st.empty())   return;
+    if(st.empty())  return;
     int tp = st.top();
     st.pop();
 
     reverseStack(st);
 
+    // there are two ways for inserting the top popped element
+    // at the bottom after the stack is reversed using recursion
+    // one is by using auxiliary stack the taken O(n) space
+    
+    // stack<int> aux;
+    // while(!st.empty()) {
+    //     aux.push(st.top());
+    //     st.pop();
+    // }
+
+    // st.push(tp);
+    // while(!aux.empty()) {
+    //     st.push(aux.top());
+    //     aux.pop();
+    // }
+
+    //another way to insert the top element at the bottom by using recursion
     insertAtBottom(st, tp);
 }
