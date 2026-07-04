9721dcd8a37597d405762b639fce25534abf77ef
Prakash Pandey <pkpndy@gmail.com>
2026-07-05 00:17:25 +0530

    Implement isAnagram function in Solution class

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())    return false;
+        unordered_map<char, int> mpp;
+
+        for(int i=0; i<s.size(); i++) {
+            mpp[s[i]]++;
+        }
+
+        for(int i=0; i<t.size(); i++) {
+            if(mpp.find(t[i]) == mpp.end()) return false;
+            mpp[t[i]]--;
+            if(mpp[t[i]]<0) return false;
+        }
+        
+        return true;
+    }
+};
