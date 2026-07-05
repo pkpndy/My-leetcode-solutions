36539b0ecb5cd1ab7eb764b7cb4874205469ae20
Prakash Pandey <pkpndy@gmail.com>
2026-07-05 23:49:23 +0530

    Enhance twoSum function with explanatory comments
    Added comments to clarify the logic of the twoSum function.

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // we will store each number with its index, so that if in future we want to 
+        // check it the required number to make target was found already once
         unordered_map<int, int> mpp;
 
         for(int i=0; i<nums.size(); i++) {
-            int need = target - nums[i];
-            if(mpp.count(need)) {
-                return {mpp[need], i};
+            int need = target - nums[i]; //the number we need to make the target
+            if(mpp.count(need)) { //if the number we need to make the target is already present in the map
+                return {mpp[need], i}; // return the index of both the numbers
             }
-            mpp[nums[i]] = i;
+            mpp[nums[i]] = i; //if not found yet then markt the index of our current number and move ahead
         }
         return {};
     }
