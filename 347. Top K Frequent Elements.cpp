c44620b98bb7a89e213bc2998d9fc90a00d50dc9
Prakash Pandey <pkpndy@gmail.com>
2026-07-06 01:22:02 +0530

    Refactor topKFrequent function implementation
    Refactor topKFrequent function to improve readability and maintainability.

--- 347. Top K Frequent Elements.cpp
+++ 347. Top K Frequent Elements.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    using Pr = pair<int, int>;
+    using Pr = pair<int, int>; //creating an alias for pair
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp;
-        vector<int> vec;
-
+        unordered_map<int, int> mpp; //create a map that will store the frequency of each number
+        vector<int> result;
         for(int it: nums) {
             mpp[it]++;
         }
 
-        priority_queue<Pr, vector<Pr>, greater<Pr>> pq;
-
-        for(auto const& it: mpp) { //use it like this as this will suit even for -ve elements in nums
-            pq.push({it.second, it.first});
-            if(pq.size() > k) {
-                pq.pop();
-            }
+        priority_queue<Pr, vector<Pr>> pq; //max-heap that will store the max numbers at the top
+        for(auto &it: mpp) {
+            pq.push({it.second, it.first}); //push in the pq according to the frequency, so the highest frequency stays at the top
         }
 
-        while(!pq.empty()) {
-            vec.push_back(pq.top().second);
+        while(k>0) {
+            auto curr = pq.top().second; //bring the actual number since they are sorted according to their frequency
+            result.push_back(curr);
             pq.pop();
+            k--;
         }
-        return vec;
+
+        return result;
     }
 };
